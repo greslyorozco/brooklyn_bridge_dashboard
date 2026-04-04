2e7301dbf9b3877c277c6c71b5926d0a589d30a5
greslyorozco <orozcog3@southernct.edu>
2026-04-03 20:13:08 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("Brooklyn Bridge Pedestrian Dashboard")
+st.subheader("Explore pedestrian traffic patterns over time")
